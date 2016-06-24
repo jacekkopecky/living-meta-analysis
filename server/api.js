6f7e0e99c07f906819a3e8cedddb2ddbbd171379
Jacek Kopecky <jacek@jacek.cz>
2016-06-24 10:56:38 +0100

    add basic user management

--- server/api.js
+++ server/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const GUARD = require('simple-google-openid').guardMiddleware({ realm: 'accounts.google.com' });
 
 const api = module.exports = express.Router({
   caseSensitive: true,
@@ -11,3 +12,43 @@ api.get('/someinfo',
   (req, res) =>
     setTimeout(() => res.json(req.user || { error: 'no user', extra: req.userError }), 500)
   );
+
+api.get(/\/profile\/([a-zA-Z.+-]+@[a-zA-Z.+-]+)/, GUARD, REGISTER_USER, returnUserProfile);
+
+
+/*
+ *         #    #  ####  ###### #####   ####
+ *         #    # #      #      #    # #
+ *         #    #  ####  #####  #    #  ####
+ *         #    #      # #      #####       #
+ *         #    # #    # #      #   #  #    #
+ *          ####   ####  ###### #    #  ####
+ */
+const users = {};
+
+function REGISTER_USER(req, res, next) {  // eslint-disable-line no-unused-vars
+  if (req.user) {
+    // todo this needs to be in a database
+    // and if it's already there, don't remove any extra properties we know about the user
+    // we could have last login?
+    users[req.user.emails[0].value] = req.user;
+    console.log('registered ' + req.user.emails[0].value);
+  }
+
+  next();
+}
+
+function returnUserProfile(req, res) {
+  const user = users[req.params[0]];
+  if (!user) {
+    res.sendStatus(404);
+  } else {
+    const retval = {
+      displayName: user.displayName,
+      name: user.name,
+      email: user.emails[0].value,
+      photos: user.photos,
+    };
+    res.json(retval);
+  }
+}
