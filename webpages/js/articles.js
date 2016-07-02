c4bcbdb04f2ac99dc0e188de451ec11acfd86bb9
Jacek Kopecky <jacek@jacek.cz>
2016-07-02 11:04:07 +0100

    handle empty list

--- webpages/js/articles.js
+++ webpages/js/articles.js
@@ -27,35 +27,39 @@
 
   function fillArticlesList() {
     var xhr = this;
+    var articles;
     if (xhr.status === 404) {
-      _.notFound();
-      return;
-    }
-    if (xhr.status > 299) {
+      articles = [];
+    } else if (xhr.status > 299) {
       window.apiFail();
       return;
     }
-    var articles = JSON.parse(xhr.responseText);
+    articles = articles || JSON.parse(xhr.responseText);
     var list = _.findEl('.article.list > ul');
     list.innerHTML = '';
-    // todo sort
-    articles.forEach(function (article) {
-      var liTemplate = _.byId('article-list-template');
-      var li = liTemplate.content.cloneNode(true);
-      _.fillEls(li, '.name', article.title);
-      _.fillEls(li, '.date', article.published);
-      _.fillEls(li, '.description', article.description);
-      if (article.tags && article.tags.length) {
-        var tags = _.findEl(li, '.tags');
-        var tagTemplate = _.byId('tag-template');
-        article.tags.forEach(function (tag) {
-          var tagEl = tagTemplate.content.cloneNode(true);
-          _.fillEls(tagEl, '.tag', tag);
-          tags.appendChild(tagEl);
-        });
-      }
-      list.appendChild(li);
-    });
+
+    if (articles.length) {
+      // todo sort
+      articles.forEach(function (article) {
+        var liTemplate = _.byId('article-list-item-template');
+        var li = liTemplate.content.cloneNode(true);
+        _.fillEls(li, '.name', article.title);
+        _.fillEls(li, '.date', article.published);
+        _.fillEls(li, '.description', article.description);
+        if (article.tags && article.tags.length) {
+          var tags = _.findEl(li, '.tags');
+          var tagTemplate = _.byId('tag-template');
+          article.tags.forEach(function (tag) {
+            var tagEl = tagTemplate.content.cloneNode(true);
+            _.fillEls(tagEl, '.tag', tag);
+            tags.appendChild(tagEl);
+          });
+        }
+        list.appendChild(li);
+      });
+    } else {
+      list.appendChild(_.byId('empty-article-list-template').content.cloneNode(true));
+    }
   }
 
 
