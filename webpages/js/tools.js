db56632ef44a7eff366a3a7924687be76b3ca022
Jacek Kopecky <jacek@jacek.cz>
2016-08-29 18:14:08 +0200

    don't change textContent if it doesn't actually change (fix for firefox which moves the cursor)

--- webpages/js/tools.js
+++ webpages/js/tools.js
@@ -69,7 +69,10 @@
       selector = root;
       root = document;
     }
-    _.findEls(root, selector).forEach(function (el) { el.textContent = valOrFun(value, el) || ''; });
+    _.findEls(root, selector).forEach(function (el) {
+      var newVal = valOrFun(value, el);
+      if (el.textContent != newVal) el.textContent = newVal || '';
+    });
   }
 
   _.setProps = function setProps(root, selector, attr, value) {
