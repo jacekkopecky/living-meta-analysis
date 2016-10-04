04399831ddcb74b7c0dfce4fc4386629cd865b1b
Jacek Kopecky <jacek@jacek.cz>
2016-10-04 10:15:39 +0100

    put google-specific configuration values in config.js

--- server/config.js
+++ server/config.js
@@ -1,4 +1,11 @@
 module.exports = {
   // this is a delay for demonstration purposes so the server seems slow
   demoApiDelay: 0,
+
+  // google-specific settings
+  gcloudProject: {
+    projectId: 'jacek-soc-port-ac-uk',
+    keyFilename: 'jacek-soc-port-ac-uk-google-key.json',
+  },
+  gcloudDatastoreNamespace: 'living-meta-analysis-v2',
 };
--- server/storage.js
+++ server/storage.js
@@ -10,12 +10,11 @@
 const tools = require('./tools');
 const ValidationError = require('./errors/ValidationError');
 
-const gcloud = require('google-cloud')({
-  projectId: 'jacek-soc-port-ac-uk',
-  keyFilename: 'jacek-soc-port-ac-uk-google-key.json',
-});
+const config = require('./config');
 
-const datastore = gcloud.datastore({ namespace: 'living-meta-analysis-v2' });
+const gcloud = require('google-cloud')(config.gcloudProject);
+
+const datastore = gcloud.datastore({ namespace: config.gcloudDatastoreNamespace });
 
 const TITLE_RE = module.exports.TITLE_RE = '[a-zA-Z0-9.-]+';
 const TITLE_REXP = new RegExp(`^${TITLE_RE}$`);
