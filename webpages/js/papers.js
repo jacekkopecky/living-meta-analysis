592e19edc1c1186d61a133b2ac8cb90517880619
Jacek Kopecky <jacek@jacek.cz>
2017-01-20 12:23:41 +0000

    review comp-visual changes

--- webpages/js/papers.js
+++ webpages/js/papers.js
@@ -641,20 +641,22 @@
             if (typeof val == 'number') val = val.toPrecision(3);
 
             _.fillEls(td, '.value', val);
+
+
+            // fill in information about where the value was computed from
             var computedFrom ="";
-            for (var i = 0; i < col.formulaColumns.length; i++) {
+            var formulaCount = col.formulaColumns.length;
+            for (var i = 0; i < formulaCount; i++) {
               var column = lima.columns[col.formulaColumns[i]];
               computedFrom += column.title;
-              // More than one left
-              if (col.formulaColumns[i+2]) {
+              if (i < formulaCount-2) { // more than one left
                 computedFrom += ", ";
               }
-              // One more left..
-              else if (col.formulaColumns[i+1]) {
+              else if (i == formulaCount-2) { // one more left..
                 computedFrom += " and ";
               }
             }
-            _.fillEls(td,  '.computedFrom', computedFrom);
+            _.fillEls(td,  '.computedfrom', computedFrom);
             _.fillEls(td, '.formula', lima.getFormulaById(col.formula).label);
           });
         }
