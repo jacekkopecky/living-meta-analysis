3a643c236c5e76fb0e61c1abad9b5c64e29b654a
Jacek Kopecky <jacek@jacek.cz>
2017-03-16 11:26:43 +0000

    tests: add test for existence of metaanalyses linked from index.html

---
+++ webpages/tests/homepage.js
@@ -0,0 +1,23 @@
+// these are tests that support the home page
+(function (window, document) { // eslint-disable-line no-unused-vars
+
+  var lima = window.lima;
+  var _ = lima._;
+
+  function test200(assert, url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false); // false for synchronous requests
+    xhr.send();
+
+    assert(xhr.status === 200, "cannot load " + url);
+  }
+
+  // test that the two meta-analyses linked from the home page are still there
+  // this could break if they are renamed to better titles and then we'd need to update the homepage
+
+  _.addTest(function testHomepageLinks(assert) {
+    test200(assert, "/api/metaanalyses/tomas.rubin@port.ac.uk/MisinformationEffect/");
+    test200(assert, "/api/metaanalyses/tomas.rubin@port.ac.uk/MyMeta-analysis/");
+  });
+
+})(window, document);
