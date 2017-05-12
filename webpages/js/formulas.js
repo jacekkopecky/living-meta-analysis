ad58e8be051e1aa1e446fdde5ad42d3a0d1defa6
Jacek Kopecky <jacek@jacek.cz>
2017-05-12 12:37:17 +0100

    metaanalysis: add chi-squared statistics
    With thanks to the internet.

--- webpages/js/formulas.js
+++ webpages/js/formulas.js
@@ -298,6 +298,12 @@
         parameters: ['values', 'weights'],
       },
       {
+        id: 'heterogeneityPValueAggr',
+        label: 'Heterogeneity P-value',
+        func: heterogeneityPValueAggr,
+        parameters: ['sum of fixed effect errors', 'degrees of freedom'],
+      },
+      {
         id: 'iSquaredAggr',
         label: 'I-squared',
         func: iSquaredAggr,
@@ -445,6 +451,14 @@
   }
 
   // this aggregate only takes single values as inputs (the parameters will likely be other aggregates)
+  function heterogeneityPValueAggr (sumfee, df) {
+    sumfee = _.strictToNumberOrNull(sumfee);
+    df = _.strictToNumberOrNull(df);
+
+    return chisq_dist_rt(sumfee, df);
+  }
+
+  // this aggregate only takes single values as inputs (the parameters will likely be other aggregates)
   function iSquaredAggr (sumfee, df) {
     sumfee = _.strictToNumberOrNull(sumfee);
     df = _.strictToNumberOrNull(df);
@@ -455,6 +469,126 @@
   // todo probable bug: if we have an aggregate with two parameters, both computed columns
   // and one of them has nulls in them
   // the whole aggregate should ignore the whole row that has a null
+
+
+  /* chi squared
+   *
+   *
+   *    ####  #    # #     ####   ####  #    #   ##   #####  ###### #####
+   *   #    # #    # #    #      #    # #    #  #  #  #    # #      #    #
+   *   #      ###### #     ####  #    # #    # #    # #    # #####  #    #
+   *   #      #    # #         # #  # # #    # ###### #####  #      #    #
+   *   #    # #    # #    #    # #   #  #    # #    # #   #  #      #    #
+   *    ####  #    # #     ####   ### #  ####  #    # #    # ###### #####
+   *
+   *
+   */
+  // todo there might be merit in publishing some statistics functions below as a javascript package
+
+
+  var chisq_dist_rt;
+
+  // adapted from gamma.js at https://github.com/substack/gamma.js
+  (function(){
+    // transliterated from the python snippet here:
+    // http://en.wikipedia.org/wiki/Lanczos_approximation
+
+    var g_ln = 607/128;
+    var p_ln = [
+      0.99999999999999709182,
+      57.156235665862923517,
+      -59.597960355475491248,
+      14.136097974741747174,
+      -0.49191381609762019978,
+      0.33994649984811888699e-4,
+      0.46523628927048575665e-4,
+      -0.98374475304879564677e-4,
+      0.15808870322491248884e-3,
+      -0.21026444172410488319e-3,
+      0.21743961811521264320e-3,
+      -0.16431810653676389022e-3,
+      0.84418223983852743293e-4,
+      -0.26190838401581408670e-4,
+      0.36899182659531622704e-5
+    ];
+
+    // Spouge approximation (suitable for large arguments)
+    function LogGamma(z) {
+
+      if(z < 0) return Number('0/0');
+      var x = p_ln[0];
+      for(var i = p_ln.length - 1; i > 0; --i) x += p_ln[i] / (z + i);
+      var t = z + g_ln + 0.5;
+      return .5*Math.log(2*Math.PI)+(z+.5)*Math.log(t)-t+Math.log(x)-Math.log(z);
+    }
+
+    // adapted from chi-squared.js at https://github.com/substack/chi-squared.js
+
+    // according to chi-squared.js: The following code liberated from
+    // http://www.math.ucla.edu/~tom/distributions/chisq.html
+
+    function Gcf(X,A) {        // Good for X>A+1
+      var A0=0;
+      var B0=1;
+      var A1=1;
+      var B1=X;
+      var AOLD=0;
+      var N=0;
+      while (Math.abs((A1-AOLD)/A1)>.00001) {
+        AOLD=A1;
+        N=N+1;
+        A0=A1+(N-A)*A0;
+        B0=B1+(N-A)*B0;
+        A1=X*A0+N*A1;
+        B1=X*B0+N*B1;
+        A0=A0/B1;
+        B0=B0/B1;
+        A1=A1/B1;
+        B1=1;
+      }
+      var Prob=Math.exp(A*Math.log(X)-X-LogGamma(A))*A1;
+      return 1-Prob
+    }
+
+    function Gser(X,A) {        // Good for X<A+1.
+      var T9=1/A;
+      var G=T9;
+      var I=1;
+      while (T9>G*.00001) {
+        T9=T9*X/(A+I);
+        G=G+T9;
+        I=I+1;
+      }
+      G=G*Math.exp(A*Math.log(X)-X-LogGamma(A));
+      return G
+    }
+
+    function Gammacdf(x,a) {
+      var GI;
+      if (x<=0) {
+        GI=0
+      } else if (x<a+1) {
+        GI=Gser(x,a)
+      } else {
+        GI=Gcf(x,a)
+      }
+      return GI
+    }
+
+    function cdf (Z, DF) {
+      if (DF<=0) {
+        throw new Error("Degrees of freedom must be positive")
+      }
+      return Gammacdf(Z/2,DF/2)
+    }
+
+    // 1-cdf gives us the CHISQ.DIST.RT Excel value, according to
+    // wikipedia https://en.wikipedia.org/wiki/Chi-squared_distribution#Table_of_.CF.872_values_vs_p-values
+    chisq_dist_rt = function(v, df) {
+      return 1-cdf(v,df);
+    };
+
+  })();
 
 
   /* parsing
