76bc2f041c3a3cea4ed7b8bed2379294fc28efde
Jacek Kopecky <jacek@jacek.cz>
2017-06-22 10:44:10 +0100

    server: add statsd monitoring
    includes only basic stats: server starting and being alive (ping every 1m); path access to API and total; error status codes

---
+++ server/stats.js
@@ -0,0 +1,52 @@
+// adopted from https://github.com/spreaker/nodejs-statsd-client
+'use strict';
+
+const dgram = require('dgram');
+
+const config = require('./config');
+
+function StatsDClient(options) {
+  options = options || {};
+  this.host = options.host || 'localhost';
+  this.port = options.port || 8125;
+  this.prefix = options.prefix || '';
+
+  // Create socket (ignore errors)
+  this.socket = dgram.createSocket('udp4');
+  this.socket.on('error', () => {});
+}
+
+StatsDClient.prototype.timing = function (bucket, value) {
+  this.send(bucket, value + '|ms');
+};
+
+StatsDClient.prototype.count = function (bucket, value, sampling) {
+  if (value == null) value = 1;
+  if (sampling == null) sampling = 1;
+  this.send(bucket, value + '|c|@' + sampling);
+};
+
+StatsDClient.prototype.increment = function (bucket) {
+  this.count(bucket, 1);
+};
+
+StatsDClient.prototype.decrement = function (bucket) {
+  this.count(bucket, -1);
+};
+
+StatsDClient.prototype.gauge = function (bucket, value) {
+  this.send(bucket, value + '|g');
+};
+
+StatsDClient.prototype.send = function (bucket, value) {
+  const buffer = new Buffer(this.prefix + bucket + ':' + value);
+
+  // Send (ignore errors)
+  this.socket.send(buffer, 0, buffer.length, this.port, this.host,
+      () => {} // ignore errors
+  );
+};
+
+
+module.exports = StatsDClient;
+module.exports.instance = new StatsDClient(config.statsdConfig);
