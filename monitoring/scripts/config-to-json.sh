5040b80f1f9dd071edaded13c90c13a714671d1f
Jacek Kopecky <jacek@jacek.cz>
2017-06-22 13:22:17 +0100

    monitoring: de-dup config

--- monitoring/scripts/config-to-json.sh
+++ monitoring/scripts/config-to-json.sh
@@ -6,16 +6,6 @@ dir=`dirname $0`
 # make sure nothing gets output by the config script, but keep the old stdout
 exec 3>&1 >/dev/null
 
-# configure the statsd server in living-meta-analysis/statsd-server-conf.sh
-# provide STATSD_HOST, STATSD_PORT, and STATSD_PREFIX
-if [ -f "$dir/../../statsd-server-conf.sh" ]
-then
-  . "$dir/../../statsd-server-conf.sh"
-fi
-
-# this needs to be kept in sync with monitor.sh
-host=${STATSD_HOST:-localhost}
-port=${STATSD_PORT:-8125}
-prefix=${STATSD_PREFIX:-test.}
+. $dir/default-config.sh
 
 echo '{"host":"'$host'","port":"'$port'","prefix":"'$prefix'"}' >&3
