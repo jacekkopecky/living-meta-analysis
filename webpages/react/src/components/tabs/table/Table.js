5d188ec259eeb09bf4d9ddb48a0ffa9e238fd84d
Yan Imensar <didzo@arch-fixe.localdomain>
2020-04-16 11:15:59 +0200

    css lint

--- webpages/react/src/components/tabs/table/Table.js
+++ webpages/react/src/components/tabs/table/Table.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Table.css';
 
 function Table() {
-  return <p>This is the table tab !</p>;
+  return <p className="tableTitle">This is the table tab !</p>;
 }
 
 export default Table;
