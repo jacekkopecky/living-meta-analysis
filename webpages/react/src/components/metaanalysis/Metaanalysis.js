257e1d18f60929e6bae8a2c59340347347117f2a
Yan Imensar <didzo@arch-fixe.localdomain>
2020-04-16 15:09:22 +0200

    little bit of styling for ma

--- webpages/react/src/components/metaanalysis/Metaanalysis.js
+++ webpages/react/src/components/metaanalysis/Metaanalysis.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Info from './info/Info';
+import './Metaanalysis.css';
 
 function Metaanalysis(props) {
   const { items } = props;
   const { title, description, published } = items;
   return (
-    <div>
-      <h1 className="title">{title}</h1>
+    <div className="metaanalysis">
+      <h1>
+        Meta-Analysis :
+        {' '}
+        {title}
+      </h1>
       <Info description={description} reference={published} />
     </div>
   );
