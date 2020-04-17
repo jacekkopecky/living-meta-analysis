cf22db66f62dc1cbea15fe5a353cec540908a6b4
Yan Imensar <didzo@arch-fixe.localdomain>
2020-04-17 16:51:06 +0200

    new components added to the App + some comments

--- webpages/react/src/index.js
+++ webpages/react/src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Render the react app on index.html div#root
 ReactDOM.render(<App />, document.querySelector('#root'));
