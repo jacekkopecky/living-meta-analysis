170fc47bc0013aabe9a55a7e5ab18b2b893f88c8
Yan Imensar <didzo@arch-fixe.localdomain>
2020-04-20 14:50:21 +0200

    start of user profile in Header

--- webpages/react/src/components/layout/header/Header.js
+++ webpages/react/src/components/layout/header/Header.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import './Header.css';
+// import img from './images/user.png';
 
 function Header() {
   return (
@@ -9,6 +10,23 @@ function Header() {
         <h1>LiMA</h1>
         <h2>Living Meta-Analysis</h2>
       </a>
+      <div className="userinfo signedoff">
+        <img src="./user.png" alt="user" className="userphoto" />
+        <div className="actions">
+          <div className="name when-signed-on">
+            {/* Signed in as: */}
+            <span className="username" />
+          </div>
+          <div className="g-signin2 signin when-signed-out" />
+          <a href="/profile" className="profile when-signed-on">Profile</a>
+          <a id="toggle-editing" className="only-if-page-about-you">
+            {/* Turn editing */}
+            {/* <span className="ifon">off</span>
+            <span className="ifoff">on</span> */}
+          </a>
+          {/* <a className="signout when-signed-on">Sign out</a> */}
+        </div>
+      </div>
     </header>
   );
 }
