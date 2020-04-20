2c9a5052166dd7edafd637727ba51c76261e4019
joolsh625 <jools.hg@gmail.com>
2020-04-20 17:05:04 +0100

    :art: fixed bad variable name

--- server/storage-dump.js
+++ server/storage-dump.js
@@ -9,9 +9,9 @@ const storageTools = require('./lib/storage-tools');
 // normal logging goes to stderr
 async function storageDump() {
   try {
-    const thing = await storageTools.dump();
+    const data = await storageTools.dump();
     console.log('storage-dump done');
-    log(JSON.stringify(thing, null, 2));
+    log(JSON.stringify(data, null, 2));
     process.exit();
   } catch (e) {
     console.error('error dumping: ', e.stack || e);
