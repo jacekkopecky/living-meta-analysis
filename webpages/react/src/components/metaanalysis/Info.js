089c3ee6b721ab183457bdfc85b977dfe01e8a4d
Yan Imensar <didzo@arch-fixe.localdomain>
2020-04-21 10:16:38 +0200

    refact: rename formatDateTime and Info has 1 subfunction

--- webpages/react/src/components/metaanalysis/Info.js
+++ webpages/react/src/components/metaanalysis/Info.js
@@ -1,34 +1,28 @@
 import React from 'react';
 import './Info.css';
 
-function Reference(props) {
-  const { value } = props;
+
+function InfoText(props) {
+  const { name, value } = props;
   return (
-    <div className="reference">
-      <p className="referenceHeader">Reference : </p>
-      <p className="referenceText">{value}</p>
+    <div className={name}>
+      <p className="header">
+        {name}
+        {' '}
+        :
+        {' '}
+      </p>
+      <p className="text">{value}</p>
     </div>
   );
 }
-
-function Description(props) {
-  const { value } = props;
-  return (
-    <div className="description">
-      <p className="descriptionHeader">Description : </p>
-      <p className="descriptionText">{value}</p>
-    </div>
-  );
-}
-
-// TODO: refactor into 1 function
 
 function Info(props) {
   const { reference, description } = props;
   return (
     <div className="info">
-      <Reference value={reference} />
-      <Description value={description} />
+      <InfoText name="reference" value={reference} />
+      <InfoText name="description" value={description} />
     </div>
   );
 }
