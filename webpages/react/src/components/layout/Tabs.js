6c80ee78aea06e8180f63bc7d0ce0dedc739de1b
Yan Imensar <didzo@arch-fixe.localdomain>
2020-04-23 18:17:15 +0200

    switched to ReactRouter for tabs & # links

--- webpages/react/src/components/layout/Tabs.js
+++ webpages/react/src/components/layout/Tabs.js
@@ -1,57 +1,25 @@
 /* eslint-disable consistent-return */
-import React, { Component } from 'react';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Tabs.css';
 
-class Tabs extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: 0,
-    };
-  }
-
-  handleSelect = (tab) => {
-    const el = this;
-    return () => el.setState({
-      active: tab,
-    });
-  }
-
-  renderTabs = () => {
-    const { active } = this.state;
-    const { children } = this.props;
-    return React.Children.map(children, (item, i) => {
-      if (i % 2 === 0) {
-        const activated = active === i ? 'active' : '';
-        return (
-          <button type="button" onClick={this.handleSelect(i)} className={`${activated}tab`}>
-            {item}
-          </button>
-        );
-      }
-    });
-  }
-
-  renderContent() {
-    const { active } = this.state;
-    const { children } = this.props;
-    return React.Children.map(children, (item, i) => {
-      if (i - 1 === active) {
-        return <div className="content">{item}</div>;
-      }
-    });
-  }
-
-  render() {
-    return (
-      <div className="tabs">
-        <div className="tabbar">
-          {this.renderTabs()}
-        </div>
-        {this.renderContent()}
-      </div>
-    );
-  }
+function Tabs() {
+  return (
+    <nav className="tabs">
+      <ul>
+        <NavLink activeClassName="active" to="/info">
+          <li>
+            Info
+          </li>
+        </NavLink>
+        <NavLink activeClassName="active" to="/table">
+          <li>
+            Table
+          </li>
+        </NavLink>
+      </ul>
+    </nav>
+  );
 }
 
 export default Tabs;
