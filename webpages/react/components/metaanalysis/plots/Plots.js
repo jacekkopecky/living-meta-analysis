46abdfd03312083b6de46e52cbbad67d148b8245
Yan <yan.imensar@gmail.com>
2020-05-13 10:19:21 +0200

    fix: group plots rect sizes. Some cleaning

--- webpages/react/components/metaanalysis/plots/Plots.js
+++ webpages/react/components/metaanalysis/plots/Plots.js
@@ -4,23 +4,33 @@ import GroupingForestPlots from './GroupingForestPlots';
 import { getSimpleForestPlotData, getGroupingForestPlotData } from '../../../tools/graphtools';
 import './Plots.css';
 
+// TODO: find keys for map()
+// TODO: add GrapeCharts
 
 function Plots(props) {
   const { graphs } = props;
   return (
     <>
+      {/* render the graphs */}
       {graphs.map((graph) => {
         const { formulaName } = graph;
-        if (formulaName === 'forestPlotPercentGraph') {
+        if (formulaName === 'forestPlotPercentGraph'
+            || formulaName === 'forestPlotNumberGraph'
+            || formulaName === 'forestPlotGraph') {
+          // Populate the graph with useful values
           getSimpleForestPlotData(graph);
           return <SimpleForestPlots forestPlots={graph} />;
         }
-        if (formulaName === 'forestPlotGroupPercentGraph') {
+        if (formulaName === 'forestPlotGroupPercentGraph'
+            || formulaName === 'forestPlotGroupNumberGraph'
+            || formulaName === 'forestPlotGroupGraph') {
+          // Populate the graph with useful values
           getGroupingForestPlotData(graph);
           return <GroupingForestPlots forestPlots={graph} />;
         }
+        // replace with GrapeCharts here
+        return 'GrapeChart';
       })}
-
     </>
   );
 }
