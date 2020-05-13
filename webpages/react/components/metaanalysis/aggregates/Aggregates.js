22f675fdeffdc5158ea41edf6f373df326a8062e
Yan <yan.imensar@gmail.com>
2020-05-13 16:33:27 +0200

    fixed use of math.js without touching formulas

--- webpages/react/components/metaanalysis/aggregates/Aggregates.js
+++ webpages/react/components/metaanalysis/aggregates/Aggregates.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import SimpleAggregates from './SimpleAggregates';
 import GroupingAggregates from './GroupingAggregates';
+import math from './../../../../lib/math.min';
 
 function Aggregates(props) {
+  // we're importing the math library used in the orginal LiMA
+  // we're putting it inside window so that formulas.js can use it (for the erf function)
+  window.math = math;
   const {
     aggregates,
     groupingAggregates,
