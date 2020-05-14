9518183896e052e2ecbdbce667a901c8573ef0fd
Jacek Kopecky <jacek@jacek.cz>
2020-05-14 13:17:40 +0100

    :hammer: move math import to top-level

--- webpages/react/index.js
+++ webpages/react/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import math from '../lib/math.min';
+
+// we're importing the math library used in the orginal LiMA
+// we're putting it inside window so that formulas.js can use it
+window.math = math;
+
 // Render the react app on index.html div#root
 window.addEventListener('load', () => {
   ReactDOM.render(<App />, document.querySelector('#root'));
