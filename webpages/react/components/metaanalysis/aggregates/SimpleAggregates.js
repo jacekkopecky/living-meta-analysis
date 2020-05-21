85dffa8c438e0c170de7a9fd2cf33ebe796e2c87
Yan <yan.imensar@gmail.com>
2020-05-21 10:28:39 +0200

    :sparkles: :construction:Details in Aggregates

--- webpages/react/components/metaanalysis/aggregates/SimpleAggregates.js
+++ webpages/react/components/metaanalysis/aggregates/SimpleAggregates.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { getAggregateDatumValue, formatNumber } from '../../../tools/datatools';
 
 function SimpleAggregates(props) {
-  const { aggregates } = props;
+  const { aggregates, toggleDisplay } = props;
   return (
     <>
       <table>
@@ -15,8 +15,16 @@ function SimpleAggregates(props) {
         <tbody>
           {aggregates.map((aggr) => {
             const value = getAggregateDatumValue(aggr, aggr.metaanalysis.papers);
+            const id = 0; // TODO: find a way to identify an aggr
+            const details = (
+              <>
+                <p>{value}</p>
+                <p>{aggr.title}</p>
+                <p>{aggr.formula}</p>
+              </>
+            );
             return (
-              <tr key={aggr.formula}>
+              <tr key={aggr.formula} onClick={() => toggleDisplay(id, details)}>
                 <td>{aggr.title}</td>
                 <td>{formatNumber(value)}</td>
               </tr>
