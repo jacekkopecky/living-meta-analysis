745a7cb054b9a9c005b1c3e6c58f47d05f0aa899
Jack Moseley <jackwmoseley@gmail.com>
2020-05-22 13:53:38 +0100

    :recycle: adding index for batch exporting

---
+++ server/errors/index.js
@@ -0,0 +1,15 @@
+const ForbiddenError = require('./ForbiddenError');
+const InternalError = require('./InternalError');
+const NotFoundError = require('./NotFoundError');
+const NotImplementedError = require('./NotImplementedError');
+const UnauthorizedError = require('./UnauthorizedError');
+const ValidationError = require('./ValidationError');
+
+module.exports = {
+  ForbiddenError,
+  InternalError,
+  NotFoundError,
+  NotImplementedError,
+  UnauthorizedError,
+  ValidationError,
+};
