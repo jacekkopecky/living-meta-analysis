20705007203a5b0f40ce61c97ee1e34fc6c3957b
DidZo13 <45854649+DidZo13@users.noreply.github.com>
2020-06-10 10:49:45 +0200

    :sparkles: live cell editing :construction: details editing

--- webpages/react/tools/editTools.js
+++ webpages/react/tools/editTools.js
@@ -1,15 +1,10 @@
 export default function replaceCell(papers, columns, value, cellId) {
-  console.log(value);
-  console.log(cellId);
-  console.log(papers);
-  const papersClone = papers;
-
+  const papersClone = [...papers];
   for (const paper of papersClone) {
     for (const exp of paper.experiments) {
       for (const col of columns) {
         if (col.id) {
           if (cellId === `${exp.ctime + paper.id}+${col.id}`) {
-            console.log('found the cell');
             const mapped = col.sourceColumnMap[exp.paper.id];
             exp.data[mapped].value = value;
           }
