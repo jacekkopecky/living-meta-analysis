689526f6c1303d691c9cd55ca0c37c05e67f3551
Jacek Kopecky <jacek@jacek.cz>
2020-07-20 19:57:59 +0100

    use SHA version

--- webpages/js/version.js
+++ webpages/js/version.js
@@ -1,20 +1,22 @@
-(function (window, document) { // eslint-disable-line no-unused-vars
+(async function (window, document) { // eslint-disable-line no-unused-vars
   'use strict';
-  var lima = window.lima;
-  var _ = lima._;  // underscore symbol is used for brevity, defined in tools.js
+  const lima = window.lima;
+  const _ = lima._;  // underscore symbol is used for brevity, defined in tools.js
 
-  var footer = _.findEl('body > footer');
+  const footer = _.findEl('body > footer');
 
   if (!footer) return;
 
-  footer.innerHTML += '<p class="limaversion">version <span class="value">&hellip;</span> (<a href="/version/log">full changelog</a>)</p>';
+  footer.innerHTML += '<p class="limaversion">version <a class="value">&hellip;</a> (<a href="/version/log">full changelog</a>)</p>';
 
-  fetch('/version', { credentials: 'same-origin' })
-  .then(_.fetchText)
-  .then(function (text) { _.fillEls('.limaversion .value', text);})
-  .catch(function (err) {
+  try {
+    const response = await fetch('/version', { credentials: 'same-origin' });
+    const text = await _.fetchText(response);
+    _.fillEls('.limaversion .value', text.substring(0, 8));
+    const url = `https://github.com/jacekkopecky/living-meta-analysis/commit/${text}`;
+    _.setProps('.limaversion .value', 'href', url);
+  } catch (err) {
     console.error("problem getting version");
     console.error(err);
-  });
-
+  }
 })(window, document);
