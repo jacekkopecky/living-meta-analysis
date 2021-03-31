82280630889326226fcc8724f032713f305bf12e
NATFINEST <ese.nat4@gmail.com>
2021-04-01 00:45:10 +0100

    Resolving Code Duplication

--- webpages-react/components/metaanalysis/plots/GraphEditor.js
+++ webpages-react/components/metaanalysis/plots/GraphEditor.js
@@ -1,77 +1,104 @@
 import React, { useState } from 'react';
 import Popup from '../Popup';
 
-function GrapeMenu(props) {
-  const {
-    graph, graphState, columns, popup,
-  } = props;
+const newParams = [null, null, null, null, null];
+const paramIndex = [null, null, null, null, null];
+const newColumnParams = [null, null, null, null, null];
+
+let menuType;
+
+function handleSubmit(e, graph, graphState, columns, popup, mType) {
+  e.preventDefault();
+
   const [graphs, setGraphs] = graphState;
-  const [popupStatus, setPopupStatus] = popup;
   const graphsClone = [...graphs];
-  const title = graph.title;
-  const params = graph.formulaParams;
-  const modCols = columns.filter((col) => col.subType === 'moderator');
-  const calcCols = columns.filter((col) => col.subType === 'calculator' || col.subType === 'calculatorN');
-  const newParams = [null, null, null, null, null];
-  const paramIndex = [null, null, null, null, null];
-  const newColumnParams = [null, null, null, null, null];
+  const type = mType;
+  let formElem;
+  const [popupStatus, setPopupStatus] = popup;
 
   function popupToggle() {
     setPopupStatus(!popupStatus);
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    let formElem;
-    if (e.currentTarget.nodeName === 'FORM') {
-      formElem = e.currentTarget;
-    } else if (e.currentTarget.nodeName === 'SELECT' || e.currentTarget.nodeName === 'INPUT') {
-      formElem = e.currentTarget.parentNode.parentNode;
-    }
-    for (const element of formElem) {
-      const input = element.children.children[0];
-      if (input && (input.nodeName === 'INPUT' || input.nodeName === 'SELECT') && input.type !== 'submit') {
-        switch (input.name) {
-        case 'grapeTitle':
-          if (input.value) {
-            graph.title = input.value;
-          }
-          break;
-        case 'grapeG1':
-          newParams[0] = input.value;
-          break;
-        case 'grapeG2':
-          newParams[2] = input.value;
-          break;
-        case 'grapeMod':
-          newParams[4] = input.value;
-          break;
-        default:
+  if (e.currentTarget.nodeName === 'FORM') {
+    formElem = e.currentTarget;
+  } else if (e.currentTarget.nodeName === 'SELECT' || e.currentTarget.nodeName === 'INPUT') {
+    formElem = e.currentTarget.parentNode.parentNode;
+  }
+  for (const element of formElem) {
+    const input = element.children.children[0];
+    if (input && (input.nodeName === 'INPUT' || input.nodeName === 'SELECT') && input.type !== 'submit') {
+      switch (input.name) {
+      case 'grapeTitle':
+      case 'forestGroupTitle':
+      case 'forestTitle':
+        if (input.value) {
+          graph.title = input.value;
         }
+        break;
+      case 'grapeG1':
+      case 'forestG1':
+      case 'forestGroupG1':
+        newParams[0] = input.value;
+        break;
+      case 'grapeG2':
+      case 'forestG2':
+      case 'forestGroupG2':
+        newParams[2] = input.value;
+        break;
+      case 'grapeMod':
+      case 'forestGroupMod':
+        newParams[4] = input.value;
+        break;
+      default:
       }
     }
-    for (let i = 0; i < newParams.length; i += 2) {
-      const colParam = columns.filter((col) => col.id === newParams[i] && col.type !== 'result')[0];
-      graph.formulaParams[i] = colParam;
-      newColumnParams[i] = colParam.title;
-      paramIndex[i] = (newParams[i] || graph.formulaParams[i].id);
-      if (i !== 4) {
-        const nColParam = columns.filter((col) => col.id === colParam.linkedN)[0];
-        graph.formulaParams[i + 1] = nColParam;
-        newColumnParams[i + 1] = nColParam.title;
-        paramIndex[i + 1] = nColParam.id;
-      }
+  }
+  for (let i = 0; i < newParams.length; i += 2) {
+    const colParam = columns.filter((col) => col.id === newParams[i] && col.type !== 'result')[0];
+    graph.formulaParams[i] = colParam;
+    newColumnParams[i] = colParam.title;
+    paramIndex[i] = (newParams[i] || graph.formulaParams[i].id);
+    if (i !== 4 && type !== 'ForestMenu') {
+      const nColParam = columns.filter((col) => col.id === colParam.linkedN)[0];
+      graph.formulaParams[i + 1] = nColParam;
+      newColumnParams[i + 1] = nColParam.title;
+      paramIndex[i + 1] = nColParam.id;
     }
+  }
+
+  if (type === 'GrapeMenu') {
     graph.formula = `grapeChartPercentGraph(${paramIndex[0]},${paramIndex[1]},${paramIndex[2]},${paramIndex[3]},${paramIndex[4]})`;
     graph.fullLabel = `Grape chart (percentages)( ${newColumnParams[0] || graph.formulaParams[0].title}, ${newColumnParams[1] || graph.formulaParams[1].title}, ${newColumnParams[2] || graph.formulaParams[2].title}, ${newColumnParams[3] || graph.formulaParams[3].title}, ${newColumnParams[4] || graph.formulaParams[4].title})`;
-    for (let i = 0; i < graphs.length; i += 1) {
-      if (graphsClone[i].id === graph.id) {
-        graphsClone[i] = graph;
-      }
-    }
-    setGraphs(graphsClone);
-    popupToggle();
+  } else if (type === 'ForestMenu') {
+    graph.formula = `forestPlotPercentGraph(${paramIndex[0]},${paramIndex[1]},${paramIndex[2]},${paramIndex[3]})`;
+    graph.fullLabel = `Forest plot (percentages)( ${newColumnParams[0] || graph.formulaParams[0].title}, ${newColumnParams[1] || graph.formulaParams[1].title}, ${newColumnParams[2] || graph.formulaParams[2].title}, ${newColumnParams[3] || graph.formulaParams[3].title})`;
+  } else if (type === 'ForestGroupMenu') {
+    graph.formula = `forestPlotGroupPercentGraph(${paramIndex[0]},${paramIndex[1]},${paramIndex[2]},${paramIndex[3]},${paramIndex[4]})`;
+    graph.fullLabel = `Forest Plot Group (percentages)( ${newColumnParams[0] || graph.formulaParams[0].title}, ${newColumnParams[1] || graph.formulaParams[1].title}, ${newColumnParams[2] || graph.formulaParams[2].title}, ${newColumnParams[3] || graph.formulaParams[3].title}, ${newColumnParams[4] || graph.formulaParams[4].title})`;
   }
+
+  for (let i = 0; i < graphs.length; i += 1) {
+    if (graphsClone[i].id === graph.id) {
+      graphsClone[i] = graph;
+    }
+  }
+  setGraphs(graphsClone);
+  popupToggle();
+}
+
+function GrapeMenu(props) {
+  const {
+    graph, graphState, columns, popup,
+  } = props;
+  const title = graph.title;
+  const params = graph.formulaParams;
+  const modCols = columns.filter((col) => col.subType === 'moderator');
+  const calcCols = columns.filter((col) => col.subType === 'calculator' || col.subType === 'calculatorN');
+
+  menuType = 'GrapeMenu';
+
+  handleSubmit(graph, graphState, columns, popup, menuType);
 
   return (
     <>
@@ -144,67 +171,13 @@ function ForestMenu(props) {
   const {
     graph, graphState, columns, popup,
   } = props;
-  const [graphs, setGraphs] = graphState;
-  const [popupStatus, setPopupStatus] = popup;
-  const graphsClone = [...graphs];
   const title = graph.title;
   const params = graph.formulaParams;
   const calcCols = columns.filter((col) => col.subType === 'calculator');
-  const newParams = [null, null, null, null];
-  const paramIndex = [null, null, null, null];
-  const newColumnParams = [null, null, null, null];
 
-  function popupToggle() {
-    setPopupStatus(!popupStatus);
-  }
+  menuType = 'ForestMenu';
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    let formElem;
-    if (e.currentTarget.nodeName === 'FORM') {
-      formElem = e.currentTarget;
-    } else if (e.currentTarget.nodeName === 'SELECT' || e.currentTarget.nodeName === 'INPUT') {
-      formElem = e.currentTarget.parentNode.parentNode;
-    }
-    for (const element of formElem) {
-      const input = element.children.children[0];
-      if (input && (input.nodeName === 'INPUT' || input.nodeName === 'SELECT') && input.type !== 'submit') {
-        switch (input.name) {
-        case 'forestTitle':
-          if (input.value) {
-            graph.title = input.value;
-          }
-          break;
-        case 'forestG1':
-          newParams[0] = input.value;
-          break;
-        case 'forestG2':
-          newParams[2] = input.value;
-          break;
-        default:
-        }
-      }
-    }
-    for (let i = 0; i < newParams.length; i += 2) {
-      const colParam = columns.filter((col) => col.id === newParams[i] && col.type !== 'result')[0];
-      graph.formulaParams[i] = colParam;
-      newColumnParams[i] = colParam.title;
-      paramIndex[i] = (newParams[i] || graph.formulaParams[i].id);
-      const nColParam = columns.filter((col) => col.id === colParam.linkedN)[0];
-      graph.formulaParams[i + 1] = nColParam;
-      newColumnParams[i + 1] = nColParam.title;
-      paramIndex[i + 1] = nColParam.id;
-    }
-    graph.formula = `forestPlotPercentGraph(${paramIndex[0]},${paramIndex[1]},${paramIndex[2]},${paramIndex[3]})`;
-    graph.fullLabel = `Forest plot (percentages)( ${newColumnParams[0] || graph.formulaParams[0].title}, ${newColumnParams[1] || graph.formulaParams[1].title}, ${newColumnParams[2] || graph.formulaParams[2].title}, ${newColumnParams[3] || graph.formulaParams[3].title})`;
-    for (let i = 0; i < graphs.length; i += 1) {
-      if (graphsClone[i].id === graph.id) {
-        graphsClone[i] = graph;
-      }
-    }
-    setGraphs(graphsClone);
-    popupToggle();
-  }
+  handleSubmit(graph, graphState, columns, popup, menuType);
 
   return (
     <form className="graphForm" onSubmit={handleSubmit}>
@@ -261,73 +234,14 @@ function ForestGroupMenu(props) {
   const {
     graph, graphState, columns, popup,
   } = props;
-  const [graphs, setGraphs] = graphState;
-  const [popupStatus, setPopupStatus] = popup;
-  const graphsClone = [...graphs];
   const title = graph.title;
   const params = graph.formulaParams;
   const modCols = columns.filter((col) => col.subType === 'moderator');
   const calcCols = columns.filter((col) => col.subType === 'calculator');
-  const newParams = [null, null, null, null, null];
-  const paramIndex = [null, null, null, null, null];
-  const newColumnParams = [null, null, null, null, null];
 
-  function popupToggle() {
-    setPopupStatus(!popupStatus);
-  }
+  menuType = 'ForestGroupMenu';
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    let formElem;
-    if (e.currentTarget.nodeName === 'FORM') {
-      formElem = e.currentTarget;
-    } else if (e.currentTarget.nodeName === 'SELECT' || e.currentTarget.nodeName === 'INPUT') {
-      formElem = e.currentTarget.parentNode.parentNode;
-    }
-    for (const element of formElem) {
-      const input = element.children.children[0];
-      if (input && (input.nodeName === 'INPUT' || input.nodeName === 'SELECT') && input.type !== 'submit') {
-        switch (input.name) {
-        case 'forestGroupTitle':
-          if (input.value) {
-            graph.title = input.value;
-          }
-          break;
-        case 'forestGroupG1':
-          newParams[0] = input.value;
-          break;
-        case 'forestGroupG2':
-          newParams[2] = input.value;
-          break;
-        case 'forestGroupMod':
-          newParams[4] = input.value;
-          break;
-        default:
-        }
-      }
-    }
-    for (let i = 0; i < newParams.length; i += 2) {
-      const colParam = columns.filter((col) => col.id === newParams[i] && col.type !== 'result')[0];
-      graph.formulaParams[i] = colParam;
-      newColumnParams[i] = colParam.title;
-      paramIndex[i] = (newParams[i] || graph.formulaParams[i].id);
-      if (i !== 4) {
-        const nColParam = columns.filter((col) => col.id === colParam.linkedN)[0];
-        graph.formulaParams[i + 1] = nColParam;
-        newColumnParams[i + 1] = nColParam.title;
-        paramIndex[i + 1] = nColParam.id;
-      }
-    }
-    graph.formula = `forestPlotGroupPercentGraph(${paramIndex[0]},${paramIndex[1]},${paramIndex[2]},${paramIndex[3]},${paramIndex[4]})`;
-    graph.fullLabel = `Forest Plot Group (percentages)( ${newColumnParams[0] || graph.formulaParams[0].title}, ${newColumnParams[1] || graph.formulaParams[1].title}, ${newColumnParams[2] || graph.formulaParams[2].title}, ${newColumnParams[3] || graph.formulaParams[3].title}, ${newColumnParams[4] || graph.formulaParams[4].title})`;
-    for (let i = 0; i < graphs.length; i += 1) {
-      if (graphsClone[i].id === graph.id) {
-        graphsClone[i] = graph;
-      }
-    }
-    setGraphs(graphsClone);
-    popupToggle();
-  }
+  handleSubmit(graph, graphState, columns, menuType, popup);
 
   return (
     <form className="graphForm" onSubmit={handleSubmit}>
